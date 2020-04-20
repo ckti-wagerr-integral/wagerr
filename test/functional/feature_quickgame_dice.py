59e13bd147cbacaa952fdd8662fb76168b2d72df
IntegralTeam V <repo@sfxdx.ru>
2020-04-20 14:51:54 +0700

    cleanup dice test

--- test/functional/feature_quickgame_dice.py
+++ test/functional/feature_quickgame_dice.py
@@ -28,9 +28,6 @@ ODDS_DIVISOR = 10000
 BETX_PERMILLE = 60
 
 class QgDiceTest(BitcoinTestFramework):
-    def get_cache_dir_name(self, node_index, block_count):
-        return ".test-chain-{0}-{1}-.node{2}".format(self.num_nodes, block_count, node_index)
-
     def get_node_setting(self, node_index, setting_name):
         with open(os.path.join(self.nodes[node_index].datadir, "wagerr.conf"), 'r', encoding='utf8') as f:
             for line in f:
@@ -41,21 +38,6 @@ class QgDiceTest(BitcoinTestFramework):
     def get_local_peer(self, node_index, is_rpc=False):
         port = self.get_node_setting(node_index, "rpcport" if is_rpc else "port")
         return "127.0.0.1:" + str(rpc_port(node_index) if port is None else port)
-
-    def sync_node_datadir(self, node_index, left, right):
-        node = self.nodes[node_index]
-        node.stop_node()
-        node.wait_until_stopped()
-        if not left:
-            left = self.nodes[node_index].datadir
-        if not right:
-            right = self.nodes[node_index].datadir
-        if os.path.isdir(right):
-            remove_tree(right)
-        copy_tree(left, right)
-        node.rpchost = self.get_local_peer(node_index, True)
-        node.start(self.extra_args)
-        node.wait_for_rpc_connection()
 
     def set_test_params(self):
         self.extra_args = None
@@ -80,35 +62,11 @@ class QgDiceTest(BitcoinTestFramework):
         self.setup_nodes()
         self.connect_network()
 
-    def save_cache(self, force=False):
-        dir_names = dict()
-        for n in range(self.num_nodes):
-            dir_name = self.get_cache_dir_name(n, self.nodes[n].getblockcount())
-            if force or not os.path.isdir(dir_name):
-                dir_names[n] = dir_name
-        if len(dir_names) > 0:
-            for node_index in dir_names.keys():
-                self.sync_node_datadir(node_index, None, dir_names[node_index])
-            self.connect_network()
-
-    def load_cache(self, block_count):
-        dir_names = dict()
-        for n in range(self.num_nodes):
-            dir_name = self.get_cache_dir_name(n, block_count)
-            if os.path.isdir(dir_name):
-                dir_names[n] = dir_name
-        if len(dir_names) == self.num_nodes:
-            for node_index in range(self.num_nodes):
-                self.sync_node_datadir(node_index, dir_names[node_index], None)
-            self.connect_network()
-            return True
-        return False
-
-    def post_qg_bet(self, bet, type_game, number = 1):
+    def post_qg_bet(self, node, player, bet, type_game, number = 1):
         mqg_bet_opcode = make_dice_bet(type_game, number)
         scriptPubKey = CScript([OP_RETURN, bytes.fromhex(mqg_bet_opcode)])
         output = CTxOut(bet, scriptPubKey)
-        post_raw_opcode(self.nodes[2], output, self.players[0])
+        post_raw_opcode(node, output, player)
 
     def check_dice(self):
         self.log.info("Check Dice Game...")
@@ -128,7 +86,6 @@ class QgDiceTest(BitcoinTestFramework):
         self.nodes[0].generate(1)
         self.sync_all()
         self.nodes[0].generate(1)
-        self.nodes[0].generate(1)
         # check players balance
         assert_equal(self.nodes[2].getbalance(), 40000)
         # check dev balance
@@ -136,19 +93,18 @@ class QgDiceTest(BitcoinTestFramework):
         # check omno balance
         assert_equal(self.nodes[3].getbalance(), 0)
 
-        self.post_qg_bet(player_bet, QG_DICE_EQUAL, 7)
-        self.post_qg_bet(player_bet, QG_DICE_NOT_EQUAL, 7)
-        self.post_qg_bet(player_bet, QG_DICE_TOTAL_OVER, 7)
-        self.post_qg_bet(player_bet, QG_DICE_TOTAL_UNDER, 7)
-        self.post_qg_bet(player_bet, QG_DICE_EVEN)
-        self.post_qg_bet(player_bet, QG_DICE_ODD)
+        self.post_qg_bet(self.nodes[2], self.players[0], player_bet, QG_DICE_EQUAL, 7)
+        self.post_qg_bet(self.nodes[2], self.players[0], player_bet, QG_DICE_NOT_EQUAL, 7)
+        self.post_qg_bet(self.nodes[2], self.players[0], player_bet, QG_DICE_TOTAL_OVER, 7)
+        self.post_qg_bet(self.nodes[2], self.players[0], player_bet, QG_DICE_TOTAL_UNDER, 7)
+        self.post_qg_bet(self.nodes[2], self.players[0], player_bet, QG_DICE_EVEN)
+        self.post_qg_bet(self.nodes[2], self.players[0], player_bet, QG_DICE_ODD)
 
         self.sync_all()
         blockHash = self.nodes[0].generate(1)[0]
         blockInfo = self.nodes[0].getblock(blockHash)
 
-        for i in range(101):
-            self.nodes[0].generate(1)
+        self.nodes[0].generate(1)
         self.sync_all()
 
         all_qg_bets = self.nodes[0].getallqgbets()
@@ -210,7 +166,6 @@ class QgDiceTest(BitcoinTestFramework):
                     assert_equal(bet['betResultType'], 'lose')
 
         assert_equal(self.nodes[2].getbalance(), 40000 + winAmount - 600) # 6 dice bets
-
     def run_test(self):
         self.check_dice()
 
